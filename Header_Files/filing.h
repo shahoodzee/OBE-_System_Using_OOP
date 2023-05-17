b95ea86ffb71923026e0f881c94fdaf5fceb52d6
shahood bin amir <93043483+shahoodzee@users.noreply.github.com>
2023-05-18 01:10:29 +0500

    Add files via upload

--- Header_Files/filing.h
+++ Header_Files/filing.h
@@ -1,1 +1,10 @@
+class filing
+{
+public:
+	virtual void writefile() {}
+	virtual void readfile() {}
+	virtual void searchfile() {}
+	virtual void deletefile() {}
 
+};
+#pragma once
---
+++ Header_Files/functionality.h
@@ -0,0 +1,16 @@
+#pragma once
+class functionality {
+public:
+	virtual void add_entity()
+	{
+		//cout << "base class" << endl;
+	}
+	virtual void update_entity()
+	{
+		//cout << "base class" << endl;
+	}
+	virtual void remove_entity()
+	{
+		//cout << "base class" << endl;
+	}
+};
